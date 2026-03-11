cccd9ee98c3d4448c2f0d1a08d74488ac7bcc87c
yiluuuuu <yilkalbewketu8@gmail.com>
2026-03-11 16:11:38 +0300

    vulnerablity check

--- server/src/index.ts
+++ server/src/index.ts
@@ -15,6 +15,7 @@ import { PrismaClient } from '@prisma/client';
 dotenv.config();
 
 const app = express();
+app.set('trust proxy', 1);
 const PORT = process.env.PORT || 5000;
 
 // Security & parsing middleware
