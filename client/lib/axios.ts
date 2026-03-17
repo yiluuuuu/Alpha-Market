76fe00c321cee710f906d98ce4d3d30752525520
yiluuuuu <yilkalbewketu8@gmail.com>
2026-03-17 12:34:43 +0300

    show error reason

--- client/lib/axios.ts
+++ client/lib/axios.ts
@@ -4,7 +4,7 @@ const api = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_URL || 'https://alpha-market-production.up.railway.app/api',
     withCredentials: true,
 });
-
+console.log("API URL:", process.env.NEXT_PUBLIC_API_URL);
 // Interceptor for JWT
 api.interceptors.request.use((config) => {
     const token = typeof window !== 'undefined' ? localStorage.getItem('alpha_token') : null;
